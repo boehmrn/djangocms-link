951a33e1e575d71e50cd3912482d00a68fa30355
Robin Böhm <robinboehm@hotmail.com>
2024-04-07 13:30:42 +0200

    added django cms 4.1 compatibility

--- djangocms_link/forms.py
+++ djangocms_link/forms.py
@@ -1,6 +1,7 @@
 from django.forms.models import ModelForm
 from django.utils.translation import gettext_lazy as _
 
+from cms.models import Page
 from djangocms_attributes_field.widgets import AttributesWidget
 
 from .fields import PageSearchField
@@ -17,8 +18,10 @@ class LinkForm(ModelForm):
         # override the internal_link fields queryset to contains just pages for
         # current site
         # this will work for PageSelectFormField
-        from cms.models import Page
-        self.fields['internal_link'].queryset = Page.objects.drafts().on_site(site)
+        # self.fields['internal_link'].queryset = Page.objects.on_site(site)
+        self.fields['internal_link'].queryset = self.get_drafts_on_site(site)
+
+        # self.fields['internal_link'].queryset = Page.objects.drafts().on_site(site)
         # set the current site as a internal_link field instance attribute
         # this will be used by the field later to properly set up the queryset
         # this will work for PageSearchField
@@ -32,3 +35,6 @@ class LinkForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['attributes'].widget = AttributesWidget()
+
+    def get_drafts_on_site(self, site):
+        return Page.objects.filter(publisher_is_draft=True, node__site=site)
